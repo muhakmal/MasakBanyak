fa8e774f7660403d5cb52803ed9b620f172f5c05
filiagestalt <silentcantabile@gmail.com>
2018-01-24 22:12:42 +0700

    First Commit

---
+++ app/src/main/java/com/baskom/masakbanyak/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.baskom.masakbanyak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.edittext_email);
+        editTextPassword = findViewById(R.id.edittext_password);
+        buttonLogin = findViewById(R.id.button_login);
+
+        editTextEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
