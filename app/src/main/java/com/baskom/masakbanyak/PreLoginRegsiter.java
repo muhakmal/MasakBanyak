71934b1beedf7e7e8a9481bc7225580bc798078e
muhamadakmal <mighty.akmal@gmail.com>
2018-04-14 15:41:09 +0700

    PreLogin & Register Activity
    -hapus LoginActivity
    -nambah PreLoginActivity sebagai pengganti LoginActivity
    -refactor kata "REGISTRATION/REGISTER" jadi "DAFTAR"
    -animasi StaggeredAnimationGroup constrain layout di PreLoginRegisterActivity dan layoutnya
    -intent dari PreLoginActivity ke MainActivity
    -set universal dimen size button_size text = 15sp
    -beberapa minor change radius_box and rectangle_box sebagai button background
    -nambahin beberapa asset vector untuk form register
    -nambah RegsiterActivity dan StaggeredAnimationGroup ke setiap formnya
    -nambah 2 gradle : StaggeredAnimationGroup, dan nLiveoDrawer (untuk ngambil library colornya)

--- app/src/main/java/com/baskom/masakbanyak/PreLoginRegsiter.java
+++ app/src/main/java/com/baskom/masakbanyak/PreLoginRegsiter.java
@@ -1,13 +1,67 @@
 package com.baskom.masakbanyak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.bartoszlipinski.constraint.StaggeredAnimationGroup;
 
 public class PreLoginRegsiter extends AppCompatActivity {
+    private StaggeredAnimationGroup gLoginForm;
+    private StaggeredAnimationGroup gLoginDaftarBtn;
+    private Button mBtnPreLogin;
+    private Button mBtnPreDaftar;
+    private Button mBtnLogin;
+    private int hideFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pre_login_regsiter);
+        setContentView(R.layout.activity_pre_login_daftar);
+        gLoginForm = findViewById(R.id.groupLoginForm);
+        gLoginDaftarBtn = findViewById(R.id.groupLoginRegisterBtn);
+        mBtnPreLogin = findViewById(R.id.btn_pre_login);
+        mBtnPreDaftar = findViewById(R.id.btn_pre_daftar);
+        mBtnLogin = findViewById(R.id.button_login);
+        gLoginForm.hide();
+
+        mBtnPreLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gLoginForm.show();
+                gLoginDaftarBtn.hide();
+                hideFlag = 1;
+            }
+        });
+
+        mBtnPreDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreLoginRegsiter.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreLoginRegsiter.this, MainActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (hideFlag == 1){
+            gLoginForm.hide();
+            gLoginDaftarBtn.show(true);
+            hideFlag = 0;
+        }else{
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/baskom/masakbanyak/RegisterActivity.java
+++ app/src/main/java/com/baskom/masakbanyak/RegisterActivity.java
@@ -2,12 +2,46 @@ package com.baskom.masakbanyak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+import com.bartoszlipinski.constraint.StaggeredAnimationGroup;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mNama;
+    private AutoCompleteTextView mEmail;
+    private AutoCompleteTextView mNoTelp;
+    private AutoCompleteTextView mAlamat;
+    private AutoCompleteTextView mKataSandi;
+    private AutoCompleteTextView mKonfirmasiSandi;
+    private StaggeredAnimationGroup gRegisterForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_daftar);
+
+        mNama = findViewById(R.id.et_nama_reg);
+        mEmail = findViewById(R.id.et_email_reg);
+        mNoTelp = findViewById(R.id.et_phone_reg);
+        mAlamat = findViewById(R.id.et_address_reg);
+        mKataSandi = findViewById(R.id.et_password_reg);
+        mKonfirmasiSandi = findViewById(R.id.et_konfirmasiPassword_reg);
+        gRegisterForm = findViewById(R.id.groupDaftarForm);
+        gRegisterForm.hide();
+        gRegisterForm.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gRegisterForm.show();
+            }
+        },500);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
